c6a0feac40997a25e0c2cc06348a7a69d5b5d95e
Lin Zhi Cheng <lin@Lins-MacBook-Air-3.local>
2022-10-17 15:40:33 +0800

    init commit lab6

---
+++ lab6/Done.java
@@ -0,0 +1,40 @@
+class Done implements Event {
+    private final Customer customer;
+    private final int serverIdx;
+    private final double time;
+
+    Done(Customer customer, int serverIdx, double finishTime) {
+        this.customer = customer;
+        this.serverIdx = serverIdx;
+        this.time = finishTime;
+    }
+
+    public Pair<Event, ImList<Server>> execute(ImList<Server> servers) {
+        Server s = servers.get(this.serverIdx);
+        s = s.popCustomer();
+        servers = servers.set(this.serverIdx, s);
+        return new Pair<Event, ImList<Server>>(new Exit(), servers);
+    }
+
+    public double getWaitTime() {
+        return 0.0;
+    }
+
+    public int getOrder() {
+        return this.customer.getId();
+    }
+
+    public double getTime() {
+        return this.time;
+    }
+
+    public String getEvent() {
+        return "DONE";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%.3f", this.time) + " " + this.customer.getId() + 
+            " done serving by " + (this.serverIdx + 1);
+    }
+}
---
+++ lab6/Server.java
@@ -0,0 +1,70 @@
+class Server {
+    private final int id;
+    private final ImList<Customer> queue;
+    private final ImList<Customer> nowServing;
+    private final int maxQ;
+    private final double finishTime;
+
+    Server(int id, int maxQ) {
+        this.id = id;
+        this.maxQ = maxQ;
+        this.queue = new ImList<Customer>();
+        this.nowServing = new ImList<Customer>();
+        this.finishTime = 0.0;
+    }
+
+    Server(Server server, ImList<Customer> nowServing, ImList<Customer> queue, double finishTime) {
+        this.id = server.id;
+        this.maxQ = server.maxQ;
+        this.nowServing = nowServing;
+        this.queue = queue;
+        this.finishTime = finishTime;
+    }
+
+    public Server addCustomer(Customer customer) {
+        ImList<Customer> queue = this.queue;
+        queue = queue.add(customer);
+        return new Server(this, this.nowServing, queue, this.finishTime);
+    }
+    
+    public Server popCustomer() {
+        ImList<Customer> nowServing = this.nowServing;
+        nowServing = nowServing.remove(0);
+        return new Server(this, nowServing, this.queue, this.finishTime);
+    }
+
+    public Pair<Server, Double> serveCustomer(double timeOfService) {
+        Customer nowServing = this.queue.get(0);
+        ImList<Customer> updatedQueue = this.queue;
+        updatedQueue = updatedQueue.remove(0);
+        ImList<Customer> updatedNowServing = this.nowServing.add(nowServing);
+        double finishTime = timeOfService + nowServing.getServiceTime();
+        return new Pair<Server, Double>(
+            new Server(this, updatedNowServing, updatedQueue, finishTime), finishTime);
+    }
+
+    public double getFinishTime() {
+        return this.finishTime;
+    }
+
+    public boolean nextInLine(Customer customer) {
+        return this.queue.get(0).getId() == customer.getId();
+    }
+    
+    public boolean canQueue() {
+        return this.queue.size() < this.maxQ;
+    }
+
+    public boolean canServe() {
+        return this.nowServing.isEmpty();
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Server " + this.getId() + ": " + this.queue.toString();
+    }
+}
