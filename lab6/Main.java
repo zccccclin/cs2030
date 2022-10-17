2d3816bf515380150ed64963fbf4dae1c548a108
Lin Zhi Cheng <lin@Lins-MacBook-Air-3.local>
2022-10-17 22:05:19 +0800

    substitute Main java

--- lab6/Main.java
+++ lab6/Main.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
 import java.util.function.Supplier;
+import java.util.Random;
+import java.util.stream.Stream;
 
 class Main {
+    private static final Random RNG_REST = new Random(3L);
+    private static final Random RNG_REST_PERIOD = new Random(4L);
+    private static final double SERVER_REST_RATE = 0.1;
+
+    static double genRestPeriod() {
+        return -Math.log(RNG_REST_PERIOD.nextDouble()) / SERVER_REST_RATE;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ImList<Pair<Double,Supplier<Double>>> inputTimes = 
@@ -10,15 +20,19 @@ class Main {
         int numOfServers = sc.nextInt();
         int qmax = sc.nextInt();
         int numOfCustomers = sc.nextInt();
+        double probRest = sc.nextDouble();
 
-        for (int i = 0; i < numOfCustomers; i++) {
-            double arrivalTime = sc.nextDouble();
-            Supplier<Double> serviceTime = () -> sc.nextDouble();
-            inputTimes = inputTimes.add(
-                new Pair<Double,Supplier<Double>>(arrivalTime, serviceTime));
-        }
+        inputTimes = new ImList<Pair<Double,Supplier<Double>>>(
+                Stream.<Pair<Double, Supplier<Double>>>generate(() -> 
+                    new Pair<Double, Supplier<Double>>(
+                        sc.nextDouble(), () -> sc.nextDouble()))
+                .limit(numOfCustomers)
+                .toList());
 
-        Simulator sim = new Simulator(numOfServers, qmax, inputTimes);
+        Supplier<Double> restTimes = () ->
+            RNG_REST.nextDouble() < probRest ? genRestPeriod() : 0.0;
+
+        Simulator sim = new Simulator(numOfServers, qmax, inputTimes, restTimes);
         System.out.println(sim.simulate());
         sc.close();
     }
