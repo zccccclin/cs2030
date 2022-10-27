2f297835d7973a6ccaa4576c8abb71e54e5d61c4
Zhicheng <linzhicheng456@gmail.com>
2022-10-27 18:22:09 +0800

    ex 6 task 2 completed

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main {
+    // Task 1
+    static IntStream twinPrimes(int n) {
+        return IntStream.rangeClosed(2, n)
+            .filter(x ->  
+                    (isPrime(x) && isPrime(x + 2)) ||
+                    (isPrime(x) && isPrime(x - 2))
+                    );
+    }
+
+    static boolean isPrime(int n) {
+        return n > 1 && IntStream
+            .range(2, n)
+            .noneMatch(x -> n % x == 0);
+    }
+
+    // Task 2
+    static String reverse(String str) {
+        return Stream.<String>of(str.split("")).reduce("", (x, y) -> y + x);
+    }
+}
