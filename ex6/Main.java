636b49941b3290ea3fc9f9f0479bb2b45aaacea7
Zhicheng <linzhicheng456@gmail.com>
2022-10-30 16:20:32 +0800

    finish ex6

--- ex6/Main.java
+++ ex6/Main.java
@@ -1,3 +1,8 @@
+import java.util.stream.Stream;
+import java.util.stream.IntStream;
+import java.util.List;
+import java.util.function.UnaryOperator;
+
 class Main {
     // Task 1
     static IntStream twinPrimes(int n) {
@@ -21,6 +26,55 @@ class Main {
 
     // Task 3
     static long countRepeats(List<Integer> list) {
-        return 0.0;   
+        return IntStream.range(1, list.size())
+            .filter(i -> i - 2 < 0 || 
+                    list.get(i - 2) != list.get(i))
+            .filter(i -> list.get(i) == list.get(i - 1))
+            .count();
+    }
+
+    //Task 4
+    static UnaryOperator<List<Integer>> generateRule() {
+        return x -> {
+            List<Boolean> change = IntStream.range(0, x.size())
+                .mapToObj(i -> changeState(x, i)).toList();
+            IntStream result = IntStream.range(0, x.size()).map(i -> {
+                if (change.get(i).booleanValue()) {
+                    return Math.abs(x.get(i) - 1);
+                } else {
+                    return x.get(i);
+                }
+            });
+            List<Integer> list = result.boxed().toList();
+            return list;
+        };
+    }
+
+    static boolean changeState(List<Integer> list, int idx) {
+        if (idx == 0) {
+            return list.get(idx) == 1 || (list.get(idx) == 0 && list.get(1) == 1);
+        }
+        int last = list.size() - 1;
+        if (idx == last) {
+            return list.get(last) == 1 || (list.get(last) == 0 && list.get(last - 1) == 1);
+        }
+        boolean scenario1 = list.get(idx) == 1;
+        boolean scenario2 = list.get(idx) == 0 && 
+            (list.get(idx - 1) == 1 && list.get(idx + 1) != 1);
+        boolean scenario3 = list.get(idx) == 0 && 
+            (list.get(idx - 1) != 1 && list.get(idx + 1) == 1);
+        return scenario1 || scenario2 || scenario3;
+    }
+
+    static Stream<String> gameOfLife(List<Integer> list, UnaryOperator<List<Integer>> rule, int n) {
+        return Stream.iterate(list, generateRule()).limit(n).map(x -> {
+            return IntStream.range(0, x.size()).mapToObj(i -> {
+                if (x.get(i) == 0) {
+                    return ".";
+                } else {
+                    return "x";
+                }
+            }).reduce("", (a, b) -> a + b);
+        });
     }
 }
