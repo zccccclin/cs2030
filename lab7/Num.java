1fe7dd61d21f52ad7bdc19fb46045a4b7f495ad2
Zhicheng <linzhicheng456@gmail.com>
2022-10-30 22:28:23 +0800

    stuck at fraction operations

--- lab7/Num.java
+++ lab7/Num.java
@@ -1,16 +1,22 @@
+import java.util.Optional;
+
 class Num extends AbstractNum<Integer> {
     protected Num(AbstractNum<Integer> absNum) {
-       super(absNum.opt);
+       super(absNum.opt.filter(AbstractNum.valid));
     }
     
     protected Num(Optional<Integer> opt) {
-        super(opt);
+        super(opt.filter(AbstractNum.valid));
     }
 
     static Num of(Integer i) {
         Optional<Integer> integer = Optional.<Integer>of(i);
         integer = integer.filter(AbstractNum.valid);
         return new Num(integer);
+    }
+
+    static Num empty() {
+        return new Num(Optional.empty());
     }
 
     static Num zero() {
@@ -26,13 +32,47 @@ class Num extends AbstractNum<Integer> {
     }
 
     public Num add(Num num) {
+        if (!num.isValid() || !this.isValid()) {
+            return Num.empty();
+        }
+        Num result = this;
         AbstractNum<Integer> absNum = 
             new AbstractNum<Integer>(num.opt.map(AbstractNum.n));
         while (!absNum.equals(Num.zero())) {
-            num = num.succ();
+            result = result.succ();
             absNum = new AbstractNum<Integer>(absNum.opt.map(AbstractNum.s));
         }
-        return num;
+        return result;
+    }
+
+    public Num mul(Num num) {
+        if (!num.isValid() || !this.isValid()) {
+            return Num.empty();
+        } 
+        Num result = Num.of(0);
+        AbstractNum<Integer> absNum = 
+            new AbstractNum<Integer>(num.opt.map(AbstractNum.n));
+        while (!absNum.equals(Num.zero())) {
+            result = result.add(this);
+            absNum = new AbstractNum<Integer>(absNum.opt.map(AbstractNum.s));
+        }
+        return result;
+    }
+    
+    public Num sub(Num num) {
+        if (!num.isValid() || !this.isValid()) {
+            return Num.empty();
+        }
+        AbstractNum<Integer> resultAbs =
+            new AbstractNum<Integer>(num.opt.map(AbstractNum.n));
+        AbstractNum<Integer> absNum = 
+            new AbstractNum<Integer>(this.opt.map(AbstractNum.n));
+        while (!absNum.equals(Num.zero())) {
+            resultAbs = new AbstractNum<Integer>(resultAbs.opt.map(AbstractNum.s));
+            absNum = new AbstractNum<Integer>(absNum.opt.map(AbstractNum.s));
+        }
+        Num result = new Num(resultAbs.opt);
+        return result;
     }
 
 }
