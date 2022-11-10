47c0c0c864bdebc556a3c9133c5e3d61d763fca7
Zhicheng <linzhicheng456@gmail.com>
2022-11-10 18:05:20 +0800

    compare and edit

--- PA2_original/Expr.java
+++ PA2_original/Expr.java
@@ -4,7 +4,6 @@ class Expr<T> {
     private final Supplier<T> value;
     private final Optional<Operator<T>> operatorStream; 
     private final Supplier<Optional<Expr<T>>> otherValue;
-    
     private Expr(Supplier<T> value, Optional<Operator<T>> operatorStream, 
         Supplier<Optional<Expr<T>>> otherValue) {
         this.value = value;
@@ -23,33 +22,16 @@ class Expr<T> {
     }
 
     public Expr<T> op(Operator<T> operator, T otherValue) {
-        Expr<T> result = op(operator, Expr.<T>of(otherValue));
-        return result;
+        return op(operator, Expr.<T>of(otherValue));
     }
     
     public Expr<T> op(Operator<T> operator, Expr<T> otherOperatedValue) {
-        Supplier<Optional<Expr<T>>> otherOperatoredValue = () -> 
-            Optional.of(Expr.of(otherOperatedValue.evaluate()));
-        Expr<T> result = this.operatorStream.map(x -> {
-            if (x.compareTo(operator) <= 0) {
-                return new Expr<T>(
-                    () -> this.evaluate(),
-                    Optional.of(operator),
-                    otherOperatoredValue);
-            } else {
-                return new Expr<T>(
-                    this.value, 
-                    this.operatorStream,
-                    () -> this.otherValue.get().map(y -> y.op(operator, otherOperatedValue)));
-            }}).orElse(new Expr<T>(this.value, Optional.of(operator), otherOperatoredValue));
-        return result;
+        Supplier<Optional<Expr<T>>> otherOperatoredValue = () -> Optional.of(Expr.of(otherOperatedValue.evaluate()));
+        return this.operatorStream.map(x -> {if (x.compareTo(operator) <= 0) {return new Expr<T>(() -> this.evaluate(), Optional.of(operator), otherOperatoredValue);} else {return new Expr<T>(this.value, this.operatorStream, () -> this.otherValue.get().map(y -> y.op(operator, otherOperatedValue)));}}).orElse(new Expr<T>(this.value, Optional.of(operator), otherOperatoredValue));
     }
 
     public T evaluate() {
-        return this.operatorStream.map(
-                x -> x.apply(this.value.get(), this.otherValue.get().map(
-                    y -> y.evaluate()).orElseThrow()))
-            .orElseGet(this.value);
+        return this.operatorStream.map(x -> x.apply(this.value.get(), this.otherValue.get().map(y -> y.evaluate()).orElseThrow())).orElseGet(this.value);
     }
 
     public String toString() {
